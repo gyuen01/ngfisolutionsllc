00a6dca6069735e1bf3e8ff6df12c5190b922f02
gyuen01 <gyuen01@yahoo.com>
2026-02-27 13:55:02 -0800

    Fixing path mismatch between config and folder structure

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig) {
 
     return {
         dir: {
-            input: ".",
+            input: "src",
             includes: "_includes",
             output: "_site"
         }
